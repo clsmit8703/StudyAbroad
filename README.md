ce3d800e700434a589f80e08fc5a4f31d635505f
Corryn <clsmit8703@ung.edu>
2014-09-30 17:22:52 -0400

    Basic files were added.
    When I initially pushed my code to GitHub, it left out some of the common files such as the __init__, generate_key, urls, and wsgi.  I also added a .gitignore so git knows what python/Django Project files to ignore.  Lastly, I created my Readme so others know what this repo is about.

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the repository for my Study Abroad in Berlin trip
---
+++ StudyAbroad/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+extra_patterns = patterns('',
+                          url(r'^', include('apps.berlin.api_urls'), name='berlin'),
+                          )
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'StudyAbroad.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'berlin/', include('apps.berlin.urls', namespace='berlin')),
+    url(r'api/v1/', include(extra_patterns, namespace='api')),
+)
